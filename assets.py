9aebdfccde76fd6ffb5b3f93f61685f35211c508
Volandofernando <volandofdo09@gmail.com>
2025-09-24 23:48:27 +0530

    Create assets.py

---
+++ assets.py
@@ -0,0 +1,8 @@
+sweatsmart-ai/"C:\Users\Hp\Downloads\Fabrics document\cotton.jpg"/cotton.jpg
+sweatsmart-ai/assets/polyester.jpg
+sweatsmart-ai/assets/nylon.jpg
+sweatsmart-ai/assets/wool.jpg
+sweatsmart-ai/assets/silk.jpg
+sweatsmart-ai/assets/linen.jpg
+sweatsmart-ai/assets/rayon.jpg
+sweatsmart-ai/assets/spandex.jpg
