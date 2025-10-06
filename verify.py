7032f2e6af489f2febcd4af7952c9cbce61e9089
Volandofernando <volandofdo09@gmail.com>
2025-10-06 16:39:49 +0530

    Update verify.py

--- verify.py
+++ verify.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.set_page_config(page_title="Google Verification")
 
-st.write("google-site-verification: google83957dc9dd572055.html")
+<meta name="google-site-verification" content="ctQWJxGy7sz--UgmKe6CmYqntLBnIUT0ItEuoYdr858" />
