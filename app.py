d2d8422123ca27c3eadbf8deab95dfb256ad11c2
Volandofernando <volandofdo09@gmail.com>
2025-10-14 21:18:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,9 @@ elif page == "🧵 AI Fabric Quiz":
     import quiz  # (if you create a quiz.py later)
 elif page == "🤖 SweatyBot":
     import sweatybot
+elif page == "🤖 SweatyBot":
+    import sweatybot
+
 
 # -------------------------------
 # Load Config
